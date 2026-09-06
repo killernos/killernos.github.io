b078b6c8bc28f0c0b4cb40d556b79981eb0710dd
KillerNoS <Ronaldsapps@gmail.com>
2026-09-06 09:33:26 -0500

    Update PS4 WebKit host next-universal-1302-research-0008

--- build.js
+++ build.js
@@ -2,8 +2,8 @@
 
 window.PS4_WEBKIT_BUILD = {
   firmwareTarget: "multi-firmware",
-  buildId: "next-universal-1302-research-0005",
-  cacheRevision: "next-universal-1302-research-0005",
+  buildId: "next-universal-1302-research-0008",
+  cacheRevision: "next-universal-1302-research-0008",
   diagnosticsSchema: "next-diagnostics-2",
   researchBuildId: "",
   flags: {
