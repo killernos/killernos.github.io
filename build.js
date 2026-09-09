005b8d5d34497518ebe57b78222fd68cfc5980a4
killernos <Ronaldsapps@gmail.com>
2026-09-08 19:57:39 -0500

    Route guide buttons to live webpage

--- build.js
+++ build.js
@@ -23,3 +23,31 @@ window.ENABLE_1302_EXPERIMENTAL = false;
     elements[i].textContent = window.PS4_WEBKIT_BUILD.buildId;
   }
 })();
+
+(function () {
+  "use strict";
+
+  function routeGuideLinksLocally() {
+    var panel = document.getElementById("docs-guide-panel");
+    if (!panel) return;
+
+    var links = panel.querySelectorAll("a.link-button");
+    var destinations = [
+      "./guide.html",
+      "./guide.html#getting-started",
+      "./guide.html#testing-1302",
+      "./guide.html#submitting-reports",
+      "./guide.html#understanding-results"
+    ];
+
+    for (var i = 0; i < links.length && i < destinations.length; i++) {
+      links[i].href = destinations[i];
+    }
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", routeGuideLinksLocally);
+  } else {
+    routeGuideLinksLocally();
+  }
+})();
