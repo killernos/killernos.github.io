33e20fed38ecd2118b8efa06f046fd3bfee32103
killernos <Ronaldsapps@gmail.com>
2026-09-20 17:11:31 -0500

    Deploy KillerNoS NEXT13 build 0024

--- build.js
+++ build.js
@@ -2,10 +2,10 @@
 
 window.PS4_WEBKIT_BUILD = {
   firmwareTarget: "multi-firmware",
-  buildId: "next-universal-1302-1352-research-0023",
-  cacheRevision: "next-universal-1302-1352-research-0023",
+  buildId: "next-universal-1302-1352-research-0024",
+  cacheRevision: "next-universal-1302-1352-research-0024",
   diagnosticsSchema: "next-diagnostics-2",
-  researchBuildId: "next-universal-1302-1352-research-0023",
+  researchBuildId: "next-universal-1302-1352-research-0024",
   flags: {
     ENABLE_1302_EXPERIMENTAL: true,
     ENABLE_NATIVE_SYSCALL_VALIDATION: true
